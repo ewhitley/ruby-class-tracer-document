80c6763fb89855f45def70557d5da1d9aee76751
Eric Whitley <ewhitley@mac.com>
2015-06-16 10:09:08 -0500

    updated hqmf json file

--- class_information.js
+++ class_information.js
@@ -1,9 +1,12 @@
 
-    var json_path = "content/class_info.json"
+    //var json_path = "content/hqmf_export.json"
+    var json_path = "content/SimpsonsProfile.json"
+    
     var class_trace_debug = false
 
     if (class_trace_debug == true) {
         $("#debug_info_tab").removeClass("hide");
+        $("#type_dependencies").removeClass("hide");
     }
 
 
@@ -19,6 +22,11 @@
 
     function strip_quotes(name) {
         return name.replace(/[\'\"]+/gi, "");
+    }
+
+    //http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
     //http://www.java2s.com/Tutorial/JavaScript/0220__Array/Usinganalphabeticalsortmethodonstrings.htm
@@ -142,11 +150,14 @@
           });
           trace_info_body.push("<dt>Referenced Types</dt>"+uq_ref_types.join(""))
 
+          times_method_called_all = data.times_method_called || 0
+
           $.each( data.classes, function( class_name, class_def ) {
 
             d3child = {}
             d3child.name = class_name
             d3child.type = "typeclass"
+            d3child.size = times_method_called_all
             d3child.children = []
             $.each ( class_def.referenced_types, function( idx, var_type){
                 if (var_type != class_name && all_class_names.indexOf(var_type) > -1) {
@@ -155,9 +166,17 @@
                     var c = {}
                     c.name = strip_quotes(var_type)
                     c.type = "typeclass"
+                    c.children = []
                     d3child.children.push(c)
                 }
             });
+            // $.each ( class_def.methods, function( method_name, method_def){
+            //   var c = {}
+            //   c.name = method_name
+            //   c.type = "typemethod"
+            //   c.size = method_def.times_method_called || 0
+            //   d3child.children.push(c)
+            // });
 
             d3json.children.push(d3child)
 
@@ -176,6 +195,8 @@
 
             var variable_header = "<div class=\"row var_head\"><div class=\"col-xs-4 col-md-3\">Name</div><div class=\"col-xs-2 col-md-1 text-center\">Options</div><div class=\"col-xs-12 col-md-8\">Type Details</div></div>"
 
+            var total_times_called = 0
+            var times_method_called_class = class_def.times_method_called || 0
 
             if (class_def.instance_vars && Object.keys(class_def.instance_vars).length > 0 ) {
                 toc_link = js_safe_class_name + "-" + js_safe_name("instance_vars")
@@ -195,7 +216,25 @@
 
                 toc_link = js_safe_class_name + "-" + js_safe_method_name
                 toc_items.push("<li><a href=\"#"+toc_link+"\">"+method_name+"</a></li>")
-                doc_items.push("<h3 id=\""+toc_link+"\">"+method_name+"</h3>")
+                
+                var method_title_block = ""
+                //doc_items.push("<h3 id=\""+toc_link+"\">"+method_name+"</h3>")
+                method_title = "<h3 id=\""+toc_link+"\">"+method_name+"</h3>"
+
+                times_method_called = method_def.times_method_called
+                method_stats = ""
+                pct_called_class = 0
+                pct_called_all = 0
+                if (times_method_called_class > 0 && times_method_called_all > 0 ) {
+                  pct_called_class = Math.round((times_method_called / times_method_called_class) * 100 )
+                  pct_called_all = Math.round((times_method_called / times_method_called_all) * 100 )
+                  method_stats = "<div class=\"stats_container\"><div><span class=\"stats_label\">Class</span><span>"+numberWithCommas(times_method_called)+" / "+numberWithCommas(times_method_called_class)+" ("+pct_called_class+"%)</span></div><div><span class=\"stats_label\">Total</span><span>"+numberWithCommas(times_method_called)+" / "+numberWithCommas(times_method_called_all)+" ("+pct_called_all+"%)</span></div></div><div class=\"clear\"/>"
+                }
+
+
+                //method_stats = "<div class=\"circliful_label\">Call Frequency</div><div class=\"circliful_container\" data-dimension=\"125\" data-text=\""+pct_called+"%\" data-info=\""+times_called+" / "+total_times_called+"\" data-width=\"10\" data-fontsize=\"14\" data-percent=\""+pct_called+"\" data-fgcolor=\"#7ea568\" data-bgcolor=\"#eee\" data-type=\"half\" data-fill=\"#ddd\"></div>"
+
+                doc_items.push("<div><div class=\"method_title\">"+method_title+"</div>"+method_stats+"</div><div class=\"clear\"/>")
 
                 if (method_def.calling_vars && Object.keys(method_def.calling_vars).length > 0 ) {
                     vars = []
@@ -243,18 +282,21 @@
           $("#debug_info_data").html("<dl class=\"dl-horizontal\">" + debug_data.join( "" ) + "</dl>")
 
           //need to call this in here since loading json is asynchronous
-          process_d3();
+          process_d3_tree();
 
           //this with the div height cheat = bad
           // should probably really just make everything tables...
           //$('[data-toggle="tooltip"]').tooltip()
 
+          $( ".circliful_container" ).each(function() {
+            $( this ).circliful();
+          });
 
         });
     }
 
 
-function process_d3() {
+function process_d3_tree() {
     //need to fix scaling
     //http://stackoverflow.com/questions/9400615/whats-the-best-way-to-make-a-d3-js-visualisation-layout-responsive
 
@@ -269,26 +311,31 @@ function process_d3() {
             return [d.y, d.x];
         });
 
+
+    root = JSON.parse( JSON.stringify(d3json) )
+
     var svg = d3.select("#d3classviz").append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
         .attr("transform", "translate(150,0)");
 
-        root = JSON.parse( JSON.stringify(d3json) );
-
-        //if (error) throw error;
-
-        var nodes = cluster.nodes(root),
-            links = cluster.links(nodes);
+        var nodes = cluster.nodes(root);
+        var links = cluster.links(nodes);
 
         var link = svg.selectAll(".link")
+            // .data(links.filter(
+            //   function(d){return d.target.type != "typemethod"})
+            // )
             .data(links)
             .enter().append("path")
             .attr("class", function(d) { return "link" + (!d.source.type ? "" : " " + d.source.type) })
             .attr("d", diagonal);
 
         var node = svg.selectAll(".node")
+            // .data(nodes.filter(
+            //   function(d){return d.type != "typemethod"})
+            // )
             .data(nodes)
             .enter().append("g")
             .attr("class", function(d) { return "node" + (!d.type ? "" : " " + d.type) })
